9dd0809a3e6077eae9faf0a2c6b13f1e423ed2fc
jianlong-yuan <yjl9122@163.com>
2022-05-27 11:46:15 +0800

    MKD

---
+++ install2.sh
@@ -0,0 +1,5 @@
+conda install pytorch=1.10.0 torchvision cudatoolkit=11.3 -c pytorch -y
+pip install mmcv-full==1.4.8 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.10.0/index.html
+pip install terminaltables imgaug onnxruntime==1.6.0 onnx albumentations POT Scikit-Image pycocotools rasterio tensorboard pillow==8.4.0
+pip install -r requirements.txt
+pip install -e .
